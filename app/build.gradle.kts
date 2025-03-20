366e7697b2eb64f6a42d973586f444152c824581
rahullohra <tgunix@gmail.com>
2025-03-20 13:05:48 +0530

    feat: lorem ipsum update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 import java.util.Properties
 
 plugins {
-//    alias(libs.plugins.android.application)
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.maven.publish)
